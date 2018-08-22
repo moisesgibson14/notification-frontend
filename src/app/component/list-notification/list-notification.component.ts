0bf19f93d8e60748c9f6c897a6e3d6b471fa2eca
moisesgibson14 <moisesjose1deinfo2@gmail.com>
2018-08-22 11:13:38 -0500

    modifico el apartado de list notifications

--- src/app/component/list-notification/list-notification.component.ts
+++ src/app/component/list-notification/list-notification.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import swal from 'sweetalert'
+import Swal from 'sweetalert2'
+import { ListNotificationsService } from '../../services/list-notifications.service';
+
+
 declare var $: any;
 
 @Component({
@@ -8,9 +11,10 @@ declare var $: any;
   styleUrls: ['./list-notification.component.css']
 })
 export class ListNotificationComponent implements OnInit {
-  public cantShowItem : any 
-  public sumCantItem :any;
-  public showMoreItemOption : boolean
+  public cantShowItem: any
+  public sumCantItem: any;
+  public showMoreItemOption: boolean;
+  public showItemMessage : boolean
   public notifications = [
     {
       title: 'Retiro / Compra Tarjeta M.N',
@@ -63,43 +67,59 @@ export class ListNotificationComponent implements OnInit {
       date: '11. 08. 2018, 11:00'
     }
   ]
-
-  constructor() {
+  constructor(public _notificationService : ListNotificationsService) {
     $(function () {
       $('[data-toggle="tooltip"]').tooltip()
     })
   }
-
   ngOnInit() {
     this.showMoreItemOption = false
+    this.showItemMessage = false
     this.sumCantItem = 0;
     this.cantShowItem = 4;
     this.getAllNotificatons()
   }
-
   getAllNotificatons() {
-    if(this.notifications){
+    this._notificationService.getNotifications().subscribe(notification =>{
+      let not = notification
+      console.log(not);
+    })
+    if (this.notifications) {
       this.showMoreItemOption = true
+      this.showItemMessage = true;
+    }else{
+
     }
+    console.log(this.notifications);
+    
   }
-
   deleteNotification() {
-
-    swal("Click on either the button or outside the modal.")
-      .then((value) => {
-        swal(`The returned value is: ${value}`);
-      });
+    Swal({
+      title: '¿Estas seguro?',
+      text: 'Esta notificación ya no se mostrará!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        console.log('si lo borro');
+        
+      }
+    })
   }
-
-  showMore(cant){
+  archiverNotification(){
+    Swal(
+      'Good job!',
+      'You clicked the button!',
+      'success'
+    )
+  }
+  showMore(cant) {
     this.sumCantItem = this.sumCantItem + cant
     this.cantShowItem = this.sumCantItem
-    console.log(this.notifications.length);
-    if(this.cantShowItem >= this.notifications.length){
-      console.log('es mayor');
+    if (this.cantShowItem >= this.notifications.length) {
       this.showMoreItemOption = false
-      
     }
   }
-
 }
--- src/app/services/list-notifications.service.ts
+++ src/app/services/list-notifications.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { Observable, of,Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListNotificationsService {
 
-  constructor() { }
+  public notifications = [
+    {
+      title: 'Retiro / Compra Tarjeta M.N',
+      description: '395 Monto $500.00 el 17/08/18 10:25:00 PM. Auto 12066...',
+      date: '17. 08. 2018, 15:00'
+    },
+    {
+      title: 'Retiro / Compra Cheques M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '13. 08. 2018, 11:00'
+    },
+    {
+      title: 'Deposito / Cuenta  M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '11. 08. 2018, 11:00'
+    },
+    {
+      title: 'Retiro / Compra Cheques M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '08. 08. 2018, 11:00'
+    },
+    {
+      title: 'Retiro / Compra Cheques M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '08. 08. 2018, 11:00'
+    },
+    {
+      title: 'Retiro / Compra Cheques M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '08. 08. 2018, 11:00'
+    },
+    {
+      title: 'Retiro / Compra Cheques M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '13. 08. 2018, 11:00'
+    },
+    {
+      title: 'Deposito / Cuenta  M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '11. 08. 2018, 11:00'
+    },
+    {
+      title: 'Retiro / Compra Cheques M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '13. 08. 2018, 11:00'
+    },
+    {
+      title: 'Deposito / Cuenta  M.N',
+      description: '499 Monto $3,300.00 el 17/08/18 09:28:00 AM Auto 88995...',
+      date: '11. 08. 2018, 11:00'
+    }
+  ]
+
+  private subject = new Subject<any>();
+
+  constructor() {
+  
+   }
+
+  getNotifications() : Observable<any>{
+    return this.subject.asObservable()
+  }
 }
